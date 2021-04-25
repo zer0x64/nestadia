f35326bda8960f09033816082e2e04b6a2cef3fd
Benoît CORTIER <benoit.cortier@fried-world.eu>
2021-04-25 01:08:19 -0400

    PPU: implement Control register

--- server/nestadia-core/src/ppu/mod.rs
+++ server/nestadia-core/src/ppu/mod.rs
@@ -1,19 +1,24 @@
 use crate::EmulatorContext;
 
+/// Registers definitions
 pub mod registers;
 
 pub type PpuFrame = [u8; 256 * 240];
 
 pub struct Ppu {
-    name_tables: [u8; 1024 * 2], // vram
-    palette_table: [u8; 32],     // for color stuff
-    oam_data: [u8; 64 * 4],      // object attribute memory, internal to PPU
+    // memory
+    name_tables: [u8; 1024 * 2], // VRAM
+    palette_table: [u8; 32],     // For color stuff
+    oam_data: [u8; 64 * 4],      // Object Attribute Memory, internal to PPU
 
-    addr_reg: registers::VramAddr, // address register pointing to name tables
+    // registers
+    addr_reg: registers::VramAddr, // Address register pointing to name tables
+    ctrl_reg: registers::ControllerReg,
 
+    // emulation-specific internal stuff
+    last_data_on_bus: u8,
     cycle_count: u16,
     scanline: i16,
-
     frame: PpuFrame,
 }
 
@@ -23,7 +28,11 @@ impl Ppu {
             name_tables: [0u8; 1024 * 2],
             palette_table: [0u8; 32],
             oam_data: [0u8; 64 * 4],
+
             addr_reg: registers::VramAddr::new(),
+            ctrl_reg: registers::ControllerReg::default(),
+
+            last_data_on_bus: 0,
             cycle_count: 0,
             scanline: 0,
             frame: [0u8; 256 * 240],
@@ -32,76 +41,113 @@ impl Ppu {
 }
 
 impl dyn EmulatorContext<Ppu> {
-    pub fn write_ppu_controls(&mut self, address: u16, data: u8) {
+    pub fn ppu_write_register(&mut self, address: u16, data: u8) {
         let address = address & 0x07; // mirror
 
-        // TODO
-
         match address {
-            0 => { /*Control*/ }
-            1 => { /*Mask*/ }
-            2 => { /*Status*/ }
-            3 => { /*OAM Address*/ }
-            4 => { /*OAM Data*/ }
-            5 => { /*Scroll*/ }
+            0 => {
+                // Write Control register
+                self.ctrl_reg = registers::ControllerReg::from_bits_truncate(data);
+            }
+            1 => {
+                // TODO: Mask
+            }
+            2 => {
+                // Status - not writable
+            }
+            3 => {
+                // TODO: OAM Address
+            }
+            4 => {
+                // TODO: OAM Data
+            }
+            5 => {
+                // TODO: Scroll
+            }
             6 => {
                 // write PPU Address
                 self.addr_reg.load(data);
             }
             7 => {
-                // read PPU Data
-                let read_addr = self.addr_reg.get();
+                // Write PPU Data
 
-                // TODO
+                // Address to write data to
+                let write_addr = self.addr_reg.get();
+
+                // All PPU data writes increment the nametable address
+                let inc_step = self.ctrl_reg.vram_addr_increment();
+                self.addr_reg.inc(inc_step);
+
+                // TODO: write to the right place
             }
             _ => {
-                // should never happen because of mask
-                unreachable!();
+                unreachable!("unexpected write to mirrored space {}", address);
             }
         }
     }
 
-    pub fn read_ppu_controls(&mut self, address: u16, _read_only: bool) -> u8 {
+    pub fn ppu_read_register(&mut self, address: u16, _read_only: bool) -> u8 {
         let address = address & 0x07; // mirror
-
-        // TODO
 
         match address {
             0 => {
-                // Control
+                // Control - not readable
                 0
             }
             1 => {
-                // Mask
+                // Mask - not readable
                 0
             }
             2 => {
-                // Status
+                // TODO: read Status
                 0
             }
             3 => {
-                // OAM Address
+                // OAM Address - not readable
                 0
             }
             4 => {
-                // OAM Data
+                // TODO: read OAM Data
                 0
             }
             5 => {
-                // Scroll
+                // Scroll - not readable
                 0
             }
             6 => {
-                // PPU Address
+                // PPU Address - not readable
                 0
             }
             7 => {
-                // PPU Data
-                0
+                // Read PPU Data
+
+                // Address to read data from
+                let read_addr = self.addr_reg.get();
+
+                // All PPU data reads increment the nametable address
+                let inc_step = self.ctrl_reg.vram_addr_increment();
+                self.addr_reg.inc(inc_step);
+
+                // PPU has to fetch the data and keep it in internal buffer.
+                // Read returns the data fetched from the previous load operation.
+                let read_data = self.last_data_on_bus;
+
+                self.last_data_on_bus = match read_addr {
+                    0..=0x1FFF => todo!("read from chr_rom"),
+                    0x2000..=0x2FFF => todo!("name tables mirroring for {}", read_addr),
+                    0x3000..=0x3EFF => {
+                        // FIXME: use log framework instead of println!
+                        println!("address space 0x3000..0x3EFF is not expected to be used, requested = {} ", read_addr);
+                        0
+                    }
+                    0x3F00..=0x3FFF => self.palette_table[usize::from(read_addr - 0x3F00)],
+                    _ => unreachable!("unexpected access to mirrored space {}", read_addr),
+                };
+
+                read_data
             }
             _ => {
-                // Should never happen because of mask
-                unreachable!();
+                unreachable!("unexpected access to mirrored space {}", address);
             }
         }
     }
@@ -115,16 +161,15 @@ impl dyn EmulatorContext<Ppu> {
             self.scanline += 1;
 
             if self.scanline >= 261 {
+                // Yeah! We got a frame ready
                 self.scanline = -1;
-
-                // TODO: write to frame
-
-                Some(&self.frame)
-            } else {
-                None
+                return Some(&self.frame);
             }
-        } else {
-            None
         }
+
+        // <-- TODO: write to frame here :)
+
+        // Frame is not ready yet
+        None
     }
 }
--- server/nestadia-core/src/ppu/registers.rs
+++ server/nestadia-core/src/ppu/registers.rs
@@ -1,3 +1,5 @@
+use bitflags::bitflags;
+
 /// VRAM address (0x0000..0x3FFF)
 pub struct VramAddr {
     value: u16,
@@ -31,14 +33,65 @@ impl VramAddr {
     }
 
     pub fn inc(&mut self, step: u8) {
-        // can't overflow
-        self.value += u16::from(step);
+        self.value = self.value.wrapping_add(u16::from(step));
         self.mirror();
     }
 
     fn mirror(&mut self) {
         const MIRRORING_HIGHER_BOUND: u16 = 0x3FFF;
         self.value &= MIRRORING_HIGHER_BOUND;
+    }
+}
+
+bitflags! {
+    pub struct ControllerReg: u8 {
+        /// N: Base nametable address lower bit
+        const NAMETABLE_ADDR_LO = 0b00000001;
+        /// N: Base nametable address higher bit
+        const NAMETABLE_ADDR_HI = 0b00000010;
+        /// N: Base nametable address bits
+        /// (0 = $2000; 1 = $2400; 2 = $2800; 3 = $2C00)
+        const NAMETABLE_ADDR = Self::NAMETABLE_ADDR_LO.bits | Self::NAMETABLE_ADDR_HI.bits;
+
+        /// I: RAM address increment per CPU read/write of PPUDATA
+        /// (0: add 1, going across; 1: add 32, going down)
+        const VRAM_ADDR_INCREMENT = 0b00000100;
+
+        /// S: Sprite pattern table address for 8x8 sprites
+        /// (0: $0000; 1: $1000; ignored in 8x16 mode)
+        const SPRITE_PATTERN_ADDR = 0b00001000;
+
+        /// B: Background pattern table address (0: $0000; 1: $1000)
+        const BACKGROUND_PATTERN_ADDR = 0b00010000;
+
+        /// H: Sprite size (0: 8x8 pixels; 1: 8x16 pixels)
+        const SPRITE_SIZE = 0b00100000;
+
+        /// P: PPU master/slave select
+        /// (0: read backdrop from EXT pins; 1: output color on EXT pins)
+        const MASTER_SLAVE_SELECT = 0b01000000;
+
+        /// V: Generate an NMI at the start of the
+        /// vertical blanking interval (0: off; 1: on)
+        const GENERATE_NMI = 0b10000000;
+    }
+}
+
+impl Default for ControllerReg {
+    fn default() -> Self {
+        Self::empty()
+    }
+}
+
+impl ControllerReg {
+    pub fn vram_addr_increment(&self) -> u8 {
+        if self.contains(Self::VRAM_ADDR_INCREMENT) {
+            // vertical mode: increment is 32 so it skips one whole nametable row.
+            32
+        } else {
+            // horizontal mode: increment is 1, so it moves to the next column.
+            1
+        }
     }
 }
 
