3a3e51348021554156617188f2ed573803e9f210
Nicolas Parr <nico.parr@hotmail.com>
2021-07-08 23:27:26 -0400

    Implemented controller 1 inputs
    Controller 2's code is commented because it doesn't seem to work for some reason. I'll need to investigate that at a later time.

--- nestadia-libretro/src/lib.rs
+++ nestadia-libretro/src/lib.rs
@@ -1,11 +1,10 @@
 #[macro_use]
 extern crate libretro_backend;
 
-extern crate nestadia;
 extern crate bitflags;
+extern crate nestadia;
 
 use bitflags::bitflags;
-
 use std::vec::Vec;
 use std::io::Read;
 use std::path::PathBuf;
@@ -20,6 +19,7 @@ const MOCK_AUDIO: [i16; 1470] = [0i16; 1470];
 bitflags! {
     #[derive(Default)]
     struct ControllerState: u8 {
+        const NONE = 0x00;
         const A = 0x80;
         const B = 0x40;
         const SELECT = 0x20;
@@ -31,9 +31,27 @@ bitflags! {
     }
 }
 
+impl ControllerState {
+    fn from(button: JoypadButton) -> ControllerState {
+        match button {
+            JoypadButton::A => ControllerState::A,
+            JoypadButton::B => ControllerState::B,
+            JoypadButton::Start => ControllerState::START,
+            JoypadButton::Select => ControllerState::SELECT,
+            JoypadButton::Down => ControllerState::DOWN,
+            JoypadButton::Left => ControllerState::LEFT,
+            JoypadButton::Right => ControllerState::RIGHT,
+            JoypadButton::Up => ControllerState::UP,
+            _ => ControllerState::NONE
+        }
+    }
+}
+
 pub struct State {
     emulator: Option<Emulator>,
     game_data: Option<GameData>,
+    controller1: ControllerState,
+    controller2: ControllerState
 }
 
 impl State {
@@ -41,6 +59,8 @@ impl State {
         State {
             emulator: None,
             game_data: None,
+            controller1: ControllerState::NONE,
+            controller2: ControllerState::NONE
         }
     }
 }
@@ -62,13 +82,16 @@ impl libretro_backend::Core for State {
             return LoadGameResult::Failed(game_data);
         }
 
-        if game_data.path().is_none() {
-            return LoadGameResult::Failed(game_data);
-        }
+        let game_data_path = match game_data.path() {
+            None => {
+                return LoadGameResult::Failed(game_data);
+            },
+            Some(path) => { path }
+        };
 
         // Get the path supplied and prepare it to be used as rom and save paths by the Core
         let mut path = PathBuf::new();
-        path.set_file_name(game_data.path().unwrap());
+        path.set_file_name(game_data_path);
 
         let mut save_path = path.clone();
         save_path.set_extension("sav");
@@ -83,20 +106,27 @@ impl libretro_backend::Core for State {
         };
 
         // Get the game data
-        let data: &[u8] = match game_data.data() {
+        let data = match game_data.data() {
             None => {
+                println!("Failed to retrieve game data");
                 return LoadGameResult::Failed(game_data);
             },
             Some(data) => { data }
         };
 
         // Create emulator instance
-        let emulator = Emulator::new(data, save_file).expect("Rom parsing failed");
+        let emulator = match Emulator::new(data, save_file) {
+            Err(_) => {
+                println!("Rom parsing failed");
+                return LoadGameResult::Failed(game_data);
+            },
+            Ok(emulator) => { emulator }
+        };
+
         self.emulator = Some(emulator);
         self.game_data = Some(game_data);
-        // return failed a la place
 
-        // Create **dummy** AV info
+        // This info is just what's expected and is static for now. We might need to change it later if need be.
         let av_info = AudioVideoInfo::new()
             .video( 256, 240, 60.00, PixelFormat::ARGB8888)
             .audio( 44100.0 )
@@ -106,49 +136,62 @@ impl libretro_backend::Core for State {
     }
 
     fn on_unload_game(&mut self) -> GameData {
-        self.game_data.take().expect("Tried to unload a game while already being unloaded.") // erreur a été called 2 fois?
+        self.game_data.take().expect("Tried to unload a game while already being unloaded.") // l'erreur est callée 2 fois?
     }
 
     fn on_run(&mut self, handle: &mut RuntimeHandle) {
-        
-        match &mut self.emulator {
+
+        let emulator = match &mut self.emulator {
             Some(emulator) => {
-
-                let frame = loop {
-                    if let Some(frame) = emulator.clock() {
-                        break frame;
-                    }
-                };
-        
-                let mut current_frame = [0u8; NUM_PIXELS * 4];
-                nestadia::frame_to_argb(&frame, &mut current_frame);
-
-                handle.upload_video_frame(&current_frame);
-                handle.upload_audio_frame(&MOCK_AUDIO);
+                emulator
             },
-            None => { }
+            None => { return; }
+        };
+
+        let frame = loop {
+            if let Some(frame) = emulator.clock() {
+                break frame;
+            }
+        };
+
+        let mut current_frame = [0u8; NUM_PIXELS * 4];
+        nestadia::frame_to_argb(&frame, &mut current_frame);
+
+        handle.upload_video_frame(&current_frame);
+        handle.upload_audio_frame(&MOCK_AUDIO);
+
+        // Reading controller inputs
+        macro_rules! update_controllers {
+            ( $( $button:ident ),+ ) => (
+                $(
+                    let controller_state: ControllerState = match ControllerState::from(JoypadButton::$button) {
+                        ControllerState::NONE => { return; },
+                        state => { state }
+                    };
+            
+                    // Setting controller 1 button state
+                    if handle.is_joypad_button_pressed(0, JoypadButton::$button) {
+                        self.controller1 |= controller_state;
+                    } else if self.controller1.contains(controller_state) {
+            
+                        self.controller1 &= !controller_state;
+                    }
+
+                    // Setting controller 2 button state
+                    /*if handle.is_joypad_button_pressed(1, JoypadButton::$button) {
+                        self.controller2 |= controller_state;
+                    } else if self.controller2.contains(controller_state) {
+            
+                        self.controller2 &= !controller_state;
+                    }*/
+                )+
+            )
         }
 
-        // idk wtf im doing
-        /*macro_rules! controller_input {
-            ( $($button:ident), +) => {
-                $(
-                    let buttonpress_id = match JoypadButton::$button {
-                        libretro_sys::DEVICE_ID_JOYPAD_A => ControllerState::A,
-                        libretro_sys::DEVICE_ID_JOYPAD_B => ControllerState::B,
-                        libretro_sys::DEVICE_ID_JOYPAD_START => ControllerState::START,
-                        libretro_sys::DEVICE_ID_JOYPAD_SELECT => ControllerState::SELECT,
-                        libretro_sys::DEVICE_ID_JOYPAD_LEFT => ControllerState::LEFT,
-                        libretro_sys::DEVICE_ID_JOYPAD_RIGHT => ControllerState::RIGHT,
-                        libretro_sys::DEVICE_ID_JOYPAD_UP => ControllerState::UP,
-                        libretro_sys::DEVICE_ID_JOYPAD_DOWN => ControllerState::DOWN
-                    };
+        update_controllers!(A, B, Up, Down, Left, Right, Select, Start);
 
-                    self.emulator.unwrap().controller_input
-                )+
-            }
-        }*/
-
+        emulator.set_controller1(self.controller1.bits());
+        // emulator.set_controller2(self.controller2.bits());
     }
 
     fn on_reset(&mut self) {
@@ -158,6 +201,22 @@ impl libretro_backend::Core for State {
             },
             None => { }
         }
+    }
+
+    fn save_memory(&mut self) -> Option<&mut [u8]> {
+        None // Not implemented
+    }
+
+    fn rtc_memory( &mut self ) -> Option< &mut [u8] > {
+        None // Not implemented
+    }
+
+    fn system_memory( &mut self ) -> Option< &mut [u8] > {
+        None // Not implemented
+    }
+
+    fn video_memory( &mut self ) -> Option< &mut [u8] > {
+        None // Not implemented
     }
 }
 
