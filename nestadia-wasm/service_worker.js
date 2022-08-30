7d9ff7c675470a164920e4c31d13956575c05918
zer0x64 <dugre.philippe@hotmail.com>
2022-08-30 16:57:15 -0400

    basic pwa support

---
+++ nestadia-wasm/service_worker.js
@@ -0,0 +1,26 @@
+var cacheName = 'nestadia';
+var filesToCache = [
+  './',
+  './index.html',
+  './nestadia-wasm.js',
+  './nestadia-wasm_bg.wasm'
+];
+
+
+/* Start the service worker and cache all of the app's content */
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+/* Serve cached content when offline */
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
